d7145be3b4045177b6ffece1d29db91547dc268f
redxef <federanko.manuel@gmail.com>
2021-02-05 15:45:17 +0100

    Create required folder.

--- start-nginx.sh
+++ start-nginx.sh
@@ -25,6 +25,7 @@ sub_env_vars="$(cat /environment_variables.txt)"
 echo "Enabling servers"
 (
     pushd "/etc/nginx/sites-available" || exit $?
+    mkdir "../sites-enabled" || exit $?
     for f in *.conf; do
         envsubst "$sub_env_vars" < "$f" > "../sites-enabled/$f"
     done
